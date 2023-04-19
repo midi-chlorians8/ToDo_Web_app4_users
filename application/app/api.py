e8cddd81c33a5db52d5d1f6ac2c8b9afbfb24f42
iu <ilya.romanovskiy.2015@mail.ru>
2023-04-19 14:53:05 +0300

    swagger

--- application/app/api.py
+++ application/app/api.py
@@ -60,7 +60,7 @@ from fastapi.staticfiles import StaticFiles
 #app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse,tags=["root"])
-async def matrix_login(request: Request):
+async def register_login(request: Request):
     return templates.TemplateResponse("log_reg_page.html", {"request": request})
 # last ok send name
 
@@ -100,7 +100,7 @@ async def get_current_user_cook(token: Optional[str] = Cookie(None)) -> dict:
 from typing import Optional
 
 @app.get("/new_page2", response_class=HTMLResponse, tags=["notes_page"])
-async def new_page(
+async def page_notes(
     request: Request,
     fullname: str = Query("Anonim"),
     current_user: Optional[UserSchema] = Depends(get_current_user_cook),
