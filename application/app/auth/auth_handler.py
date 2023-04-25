2166f325fb14f8cb21e0398ad8745499864958e4
midi-chlorians8 <ilya.romanovskiy.2015@mail.ru>
2023-04-26 00:09:43 +0300

    signJWT correct

--- application/app/auth/auth_handler.py
+++ application/app/auth/auth_handler.py
@@ -8,22 +8,43 @@ from decouple import config
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
 
+from typing import Optional
+from datetime import timedelta, datetime
 
 def token_response(token: str):
     return {
         "access_token": token
     }
 
-
-def signJWT(user_id: str) -> Dict[str, str]:
+def signJWT(user_id: str, expires_delta: Optional[timedelta] = None) -> Dict[str, str]:
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(hours=24)
+    
     payload = {
         "user_id": user_id,
-        #"expires": time.time() + 600
-        "expires": time.time() + 60 * 60 * 24
+        "expires": expire.timestamp()
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
     return token_response(token)
+
+# def token_response(token: str):
+#     return {
+#         "access_token": token
+#     }
+
+
+# def signJWT(user_id: str) -> Dict[str, str]:
+#     payload = {
+#         "user_id": user_id,
+#         #"expires": time.time() + 600
+#         "expires": time.time() + 60 * 60 * 24
+#     }
+#     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+#     return token_response(token)
 
 
 def decodeJWT(token: str) -> dict:
