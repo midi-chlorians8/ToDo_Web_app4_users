fd66fdfa05dfa1c8ffcd6caa04cbb004159b960d
e.kim <e.kim@mx.mdcm.co>
2023-05-08 15:39:11 +0300

    fix requirements.txt

--- application/app/models/user.py
+++ application/app/models/user.py
@@ -1,11 +1,13 @@
 from typing import Optional, List
 from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy import String
+from sqlalchemy.sql.schema import Column
 from pydantic import EmailStr
 from app.models.posts import Posts
 
 
 class User(SQLModel, table=True):
     user_id: Optional[int] = Field(default=None, primary_key=True)
-    email: EmailStr = Field(unique=True)
+    email: EmailStr = Field(sa_column=Column("email", String, unique=True))
     psw: str
     posts: List['Posts'] = Relationship()
