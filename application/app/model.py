a3b015ce9bb6b7997e31e6b445a16ad7a1570231
midi-chlorians8 <ilya.romanovskiy.2015@mail.ru>
2023-05-13 02:42:49 +0300

    vernul

---
+++ application/app/model.py
@@ -0,0 +1,72 @@
+from typing import Optional
+from pydantic import BaseModel, Field, EmailStr
+
+class PostSchema(BaseModel):
+    id: int = Field(default=None)
+    # title: str = Field(...)
+    content: str = Field(...)
+    owner_id: str = Field(...)
+    created_at: str | None = None
+    # created_at: str = Field(...)
+    # created_at: str | None = Field(None, ...)
+
+
+    class Config:
+        schema_extra = {
+            "example": {
+                # "title": "Securing FastAPI applications with JWT.",
+                "content": "In this tutorial, you'll learn how to secure your application by enabling authentication using JWT. We'll be using PyJWT to sign, encode and decode JWT tokens....",
+                "owner_id": " ",
+                "created_at": "2023-05-01T15:02:35.123456" # example value
+            }
+        }
+
+class UserSchema(BaseModel):
+    # fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+    #reset_token: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                # "fullname": "Abdulazeez Abdulazeez Adeshina",
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "abdulazeez@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+#add email pass recovery
+class PasswordResetSchema(BaseModel):
+    email: EmailStr = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "user@example.com"
+            }
+        }
+
+class PasswordUpdateSchema(BaseModel):
+    token: str = Field(...)
+    new_password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "token": "your_token_here",
+                "new_password": "new_password"
+            }
+        }
